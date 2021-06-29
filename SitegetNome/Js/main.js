e6ba9b0fa4a7a2b9032a31b93e1f9fc88d0ee1a0
Lukas Feitosa <lukynnhas.feitosa@gmail.com>
2021-06-29 14:29:53 -0300

    Add files via upload

---
+++ SitegetNome/Js/main.js
@@ -0,0 +1,8 @@
+
+function submit() {
+    var input = document.getElementById('name');
+    var texto = input.value;
+    console.log("Nome inserido: " + texto);
+
+    alert("Olá Sr. " + texto + " Balalaika");
+}
