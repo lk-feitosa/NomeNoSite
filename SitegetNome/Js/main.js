eae934f7c85d11c35535abf1beead1d55716a9c9
Lukas Feitosa <lukynnhas.feitosa@gmail.com>
2021-06-29 16:30:41 -0300

    Update main.js

--- SitegetNome/Js/main.js
+++ SitegetNome/Js/main.js
@@ -2,7 +2,12 @@
 function submit() {
     var input = document.getElementById('name');
     var texto = input.value;
+
     console.log("Nome inserido: " + texto);
 
-    alert("Olá Sr. " + texto + " Balalaika");
+    if ( isNaN(texto) ) {
+        alert("Olá Sr. " + texto + " Balalaika");
+    } else {
+        alert("Não coloque números");
+    }
 }
